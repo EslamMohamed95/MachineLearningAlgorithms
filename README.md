2cb7d83ca47a76014cd49d82e090513436f21798
EslamMohamed95 <islam_hikal@hotmail.com>
2022-07-31 18:35:58 +0200

    update README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MachineLearningAlgorithms
+
+This repo is a collection of algorithms I implemented from scratch during my learning journey from different sources.
+will keep it updated and open to everyone to get to learn like I did, hope this is useful for someone out there.
+
--- RandomForest.py
+++ RandomForest.py
@@ -7,17 +7,17 @@ from DecisionTree import DecisionTree
 
 def bootstrap_sample(X, y):
     n_samples = X.shape[0]
-    idxs = np.random.choice(n_samples, size=n_samples, replace=True)
+    idxs = np.random.choice(n_samples, n_samples, replace=True)
     return X[idxs], y[idxs]
 
 
 def most_common_label(y):
     counter = Counter(y)
-    return counter.most_common(1)[0]
+    return counter.most_common(1)[0][0]
 
 
 class RandomForest:
-    def __init__(self, n_trees=100, min_samples_split=2, max_depth=100, n_feats=None):
+    def __init__(self, n_trees=10, min_samples_split=2, max_depth=100, n_feats=None):
         self.n_trees = n_trees
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
@@ -27,16 +27,19 @@ class RandomForest:
     def fit(self, X, y):
         self.trees = []
         for _ in range(self.n_trees):
-            tree = DecisionTree(min_samples_split=self.min_samples_split,
-                                max_depth=self.max_depth, n_feats=self.n_feats)
-            X_sample, y_sample = bootstrap_sample(X, y)
-            tree.fit(X_sample, y_sample)
+            tree = DecisionTree(
+                min_samples_split=self.min_samples_split,
+                max_depth=self.max_depth,
+                n_feats=self.n_feats,
+            )
+            X_samp, y_samp = bootstrap_sample(X, y)
+            tree.fit(X_samp, y_samp)
             self.trees.append(tree)
 
     def predict(self, X):
         tree_preds = np.array([tree.predict(X) for tree in self.trees])
-        tree_preds = np.swapaxes(tree_preds, 0, 1)
-        y_pred = [most_common_label(tree_preds) for _ in tree_preds]
+        tree_preds_swap = np.swapaxes(tree_preds, 0, 1)
+        y_pred = [most_common_label(tree_pred) for tree_pred in tree_preds_swap]
         return np.array(y_pred)
 
 
